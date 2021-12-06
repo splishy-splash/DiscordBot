1a3b5011e205f4433f6a7321be88aa86a4ddf2d5
splishy-splashy <nathankyle88@hotmail.com>
2021-12-06 00:01:58 -0500

    '!snow' will now take multiple default zip codes separated by a space in the config file.

--- weather.py
+++ weather.py
@@ -3,32 +3,58 @@ import json
 import datetime
 
 
-def check_weather(zip):
+def check_weather(zipcode_list):
     snow_times = []
-    if zip == ():
+    if zipcode_list == ():
         with open('weather_config.txt', 'r') as f:
-            zipcode = f.readline()
+            zipcode = f.readline().split()
+            print(zipcode)
             rapidapikey = f.readline()
+            for zip in zipcode:
+                count = 0
+                snow_times.append(zip)
+                url = "https://community-open-weather-map.p.rapidapi.com/forecast"
+                querystring = {"units": "standard", "zip": zip.strip()}
+
+                headers = {
+                    'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com",
+                    'x-rapidapi-key': rapidapikey.strip()
+                }
+
+                response = requests.request("GET", url, headers=headers, params=querystring)
+                response_json = json.loads(response.text)
+                for i in response_json['list']:
+                    if i['weather'][0]['main'] == 'Snow':
+                        snow_times.append(datetime.datetime.fromtimestamp(int(i['dt'])).isoformat())
+                        count += 1
+                if count == 0:
+                    snow_times.append('No snow!')
+            return snow_times
+
     else:
-        zipcode = ''.join(zip)
+        count = 0
+        zipcode = ''.join(zipcode_list)
         with open('weather_config.txt', 'r') as f:
             rapidapikey = f.readlines()[1]
+        snow_times.append(zipcode)
+        url = "https://community-open-weather-map.p.rapidapi.com/forecast"
+        querystring = {"units":"standard","zip":zipcode.strip()}
 
-    url = "https://community-open-weather-map.p.rapidapi.com/forecast"
-    querystring = {"units":"standard","zip":zipcode.strip()}
+        headers = {
+            'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com",
+            'x-rapidapi-key': rapidapikey.strip()
+            }
 
-    headers = {
-        'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com",
-        'x-rapidapi-key': rapidapikey.strip()
-        }
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        response_json = json.loads(response.text)
+        for i in response_json['list']:
+            if i['weather'][0]['main'] == 'Snow':
+                snow_times.append(datetime.datetime.fromtimestamp(int(i['dt'])).isoformat())
+                count += 1
 
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    response_json = json.loads(response.text)
-    for i in response_json['list']:
-        if i['weather'][0]['main'] == 'Snow':
-            snow_times.append(datetime.datetime.fromtimestamp(int(i['dt'])).isoformat())
+        if count == 0:
+            snow_times.append('No snow!')
     return snow_times
-
 
 
 def format_message(snow_list):
